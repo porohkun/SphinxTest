22a58c529a5319601133ec021eba7639ed7373a8
Pavel <5593638+porohkun@users.noreply.github.com>
2015-11-12 15:55:29 +0300

    initial

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Settings : MonoBehaviour
+{
+    public Point CellSize;
+    public float SegmentMinLength = 5f;
+
+    void Start()
+    {
+        Global.LoadSettings(this);
+    }
+
+}
